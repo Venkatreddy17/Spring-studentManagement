0710f9e3607cbf996dc9a902d51d4f96044c3925
Shivannagarivenkat1706@gmail.com <shivannagarivenkat1706@gmail.com>
2024-11-07 14:03:49 +0530

    add view Resolver

--- src/main/java/com/seleniumexpress/sm/config/StudentAppConfig.java
+++ src/main/java/com/seleniumexpress/sm/config/StudentAppConfig.java
@@ -1,12 +1,23 @@
 package com.seleniumexpress.sm.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
 @EnableWebMvc
-@ComponentScan(basePackages = "com.seleniumexpress")
+@ComponentScan(basePackages = {"com.seleniumexpress"})
 public class StudentAppConfig {
-
+   
+	@Bean
+	public InternalResourceViewResolver viewResolver() {
+		
+		InternalResourceViewResolver viewResolver=new InternalResourceViewResolver();
+		viewResolver.setPrefix("/WEB-INF/view/");
+		viewResolver.setSuffix(".jsp");
+		
+		return viewResolver;
+	}
 }
--- src/main/java/com/seleniumexpress/sm/controllers/StudentController.java
+++ src/main/java/com/seleniumexpress/sm/controllers/StudentController.java
@@ -1,15 +1,16 @@
 package com.seleniumexpress.sm.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
 
 @Controller
 public class StudentController {
    
-	@ResponseBody
-	@RequestMapping("/showStudent")
+	
+	@GetMapping("/showStudent")
 	public String showStudentList() {
-		return "student-list data!";
+		return "student-list";
 	}
 }
